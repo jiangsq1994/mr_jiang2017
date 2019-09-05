43d941d041bc14f2f1be8cbfb5721360a3e16178
名字 <wy756876419@163.com>
2019-09-05 09:09:04 +0800

    test_ui.py

--- test_case/ui/mall/test_ui.py
+++ test_case/ui/mall/test_ui.py
@@ -1,19 +1,13 @@
 from selenium import webdriver
-from tools import request_tool
-from tools import assert_tool
-from tools import random_tool
-from tools import mysql_tool
-from tools import excel_tool
-from tools import log_tool
-from tools import os_tool
-from tools import shell_tool
-import pytest
-import allure
+
+# 项目根目录建config包，里面建conf.py文件，用于配置
+from config import conf
+
+
 # 项目根目录建config包，里面建conf.py文件，用于配置
 from config import conf
 from config.conf import DRIVER_PATH, url
 from tool.base_ui import BaseUI
-
 dr = webdriver.Chrome(DRIVER_PATH)
 dr.maximize_window()  # 最大化浏览器
 dr.implicitly_wait(8)  # 设置隐式时间等待
@@ -22,3 +16,4 @@ base = BaseUI(dr)
 
 
 
+# https://github.com/LudvikWoo/guoya-pycharm-settings.git
